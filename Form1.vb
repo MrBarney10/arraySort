fec29e46d18fad73d851c0337ec88a27d4b86b4e
Wan Azim Suhaimi Bin Wan Abdul Aziz <18DDT19F1134@student.ptss.edu.my>
2021-04-14 12:55:56 +0800

    Sorting

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,16 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strNamaHari(6) As String
+        strNamaHari(0) = "Ahad"
+        strNamaHari(1) = "Isnin"
+        strNamaHari(2) = "Selasa"
+        strNamaHari(3) = "Rabu"
+        strNamaHari(4) = "Khamis"
+        strNamaHari(5) = "Jumaat"
+        strNamaHari(6) = "Sabtu"
+        Array.Sort(strNamaHari)
+        For i = 0 To 6
+            ListBox1.Items.Add(strNamaHari(i))
+        Next
+    End Sub
 End Class
